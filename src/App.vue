31115abf254568597a2edb3bf5582fa3bce3f99f
hyeryun3 <hryun3@gmail.com>
2023-02-06 16:07:56 +0900

    [23.02.06] vue axios 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
   <LoginPage/>
-  <div>
-    <h3>{{ a }}</h3>
-    <h3>{{ b }}</h3>
-    
-  </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import LoginPage from './components/LoginPage.vue'
 import axios from 'axios'
 
@@ -17,37 +11,20 @@ export default {
   components: {
     LoginPage
   },
-  data(){
-    return {
-      a:'',
-      b:''
-    }
-  },
   methods: {
-    aa(){
-      const url = '/api/aa'
-      axios.get(url).then((res) => {
-        console.log(res)  
-        this.a = res.data.id
-        this.b = res.data.pw     
-      }).catch((err)=>{
-        console.log(err)
-      })
-    },
     home(){
       const url = '/api/home'
       axios.get(url).then((res) => {
-        console.log(res)
+        console.log('home() 성공')
+        console.log(res.data)
+      }).catch((err) => {
+        console.log('home() 실패')
+        console.log(err)
       })
     }
   },
   mounted(){
-    this.aa()
     this.home()
   }
 }
 </script>
-
-<style>
-
-</style>
--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -4,46 +4,45 @@
         <span>PW</span> <input type="password" size="10" maxlength="12" v-model="pw"/>
     </div>
     <div>
+      {{ id }} {{ pw }}
+    </div>
+    <div>
       <button href="javascript:;" @click.prevent="check">체크</button>
     </div>
-  </template>
+</template>
   
-  <script>
-  import axios from 'axios'
-  
-  export default {
-    name: 'LoginPage',
-    data() {
+<script>
+import axios from 'axios'
 
-    },
-    methods: {
-      check(){
-        console.log(this.id)
-        axios.post('/api/check',{
-          id: this.id,
-          pw: this.pw
-        }).then((res) => {
-          console.log(res)          
-          
-        }).catch((err)=>{
-          console.log(err)
-        })
-      }
+export default {
+  name: 'LoginPage',
+  data() {
+    return{
+      id:'',
+      pw:''
+    }
+  },
+  methods: {
+    check(){
+      axios.post('/api/check',{
+        id: this.id,
+        pw: this.pw
+      }).then((res) => {
+        console.log('check() 성공')
+        this.id = res.data.id
+        this.pw = res.data.pw
+      }).catch((err)=>{
+        console.log('check() 실패')
+        console.log(err)
+      })
     }
   }
-  </script>
+}
+</script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  span {
-    margin: 5px;
-  }
-  input {
-    margin: 5px 20px;
-    color: #42b983;
-  }
-  button {
-    width: 100px;
-  }
-  </style>
+<style scoped>
+  @import url(../assets/css/Main.css);
+
+</style>
   
