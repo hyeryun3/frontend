cdbc7137454c25b2f98f6c57fa62bdcc3812b8f1
hyeryun3 <hryun3@gmail.com>
2023-02-13 16:14:14 +0900

    [23.02.13] ..

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,10 @@
 <template>
   <router-view/>
-  <!-- <JoinPage/> -->
 </template>
 
 <script>
-// import JoinPage from './components/JoinPage.vue'
-// import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    // JoinPage
-  },
-  // methods: {
-  //   home(){
-  //     const url = '/api/home'
-  //     axios.get(url).then((res) => {
-  //       console.log('home() 성공')
-  //       console.log(res.data)
-  //     }).catch((err) => {
-  //       console.log('home() 실패')
-  //       console.log(err)
-  //     })
-  //   },
-  // },
-  // mounted(){
-  //   this.home()
-  // }
 }
 </script>
